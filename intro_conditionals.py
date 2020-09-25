adadd66fc447f4740a961658466705195b5466ef
mfcust <43455391+mfcust@users.noreply.github.com>
2020-09-24 18:38:09 -0700

    Update intro_conditionals.py

--- intro_conditionals.py
+++ intro_conditionals.py
@@ -11,3 +11,22 @@
 #Here is the exact same code, but there is an error that doesn't allow it to run how you want it to. Fix the error!
 #HINT: Spacing and indentation matters! Code located at the same indentation position is considered to be in the same "block" of code
 #HINT 2: 4 spaces is the same as one tab. Code inside an IF statement needs to be indented!
+answer = input("Are you hungry? ")
+
+if answer == "yes":
+print("I made you some lunch!")
+  
+if answer == "no":
+print("I'll put your food in the fridge until you're ready!")
+      
+
+ #You can have multiple lines of code inside of each conditional IF statement. Try adding another print statement to the end
+ #of each conditional statement:
+      
+      
+      
+      
+      
+      
+ #Conditions help us compare things! We compare things by using relational operators! Did you notice we are using a
+#double equals (==) sign. This is fundamentally different than using one equals sign.
