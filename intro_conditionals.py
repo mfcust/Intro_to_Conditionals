2836a5d4f32de77b2999f58debeeee7072ed6b67
mfcust <43455391+mfcust@users.noreply.github.com>
2020-09-24 17:49:45 -0700

    Create intro_conditionals.py

---
+++ intro_conditionals.py
@@ -0,0 +1,13 @@
+#Copy and paste the block of the code from markdown (.md) file and type out what you observe happens when you run your program.
+#What happens when you respond with "yes"? What happens when you respond with "no"? What if you resond with something completely different?
+#Use a hashtag(#) to type out your answers.
+
+
+
+
+
+
+
+#Here is the exact same code, but there is an error that doesn't allow it to run how you want it to. Fix the error!
+#HINT: Spacing and indentation matters! Code located at the same indentation position is considered to be in the same "block" of code
+#HINT 2: 4 spaces is the same as one tab. Code inside an IF statement needs to be indented!
